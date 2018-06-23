8b0ff641b40449d30bd3057ce06e18e7875c2dd0
Glendleton <gpendlet92@gmail.com>
2018-06-23 16:47:40 -0700

    partially done

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// step6/13) change the background-color of the div to a different color.
+document.getElementById("donkey").style.background = "blue";
+// step7/13) create a div and give it an ID.
+function addElement () {
+    var newDiv = document.createElement("div");
+    var newId = document.createId("dancer");
+}
+// step8/13) create an h1tag and add some text to it using the textContent property.
+
+// step9/13)add that h1 to the div.
+
+// step10/13) create another div and give it an ID.
+
+// step11/13) create an h1 element. create a text node. add the text node to your h1, then add your h1 to your div.
+
+// step12/13) create an image and a button tag.
+
+// step13/13) createa  function that changes the img source and alt attributes when you click on that button
